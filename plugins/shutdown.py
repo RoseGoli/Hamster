baae2d66715963cf892e817f1202d6da36e8c098
RoseGoli <mytlgoli@gmail.com>
2024-07-01 19:30:16 +0330

    change stop tasks

--- plugins/shutdown.py
+++ plugins/shutdown.py
@@ -1,8 +1,22 @@
+import asyncio
 from telethon import events
 from src.config import settings
 
 async def init(bot):
     @bot.on(events.NewMessage(pattern='Shutdown🔥', from_users=settings.OWNERS))
     async def handler(event):
+        loop      = asyncio.get_event_loop()
+        all_tasks = asyncio.all_tasks(loop=loop)
+        tasks     = [
+            task for task in all_tasks
+            if isinstance(task, asyncio.Task) and task._coro.__name__ == 'run'
+        ]
+
+        for task in tasks:
+            try:
+                task.cancel()
+            except:
+                ...
+
         await event.respond('<b>Shutted Down !</b>')
         bot.disconnect()
